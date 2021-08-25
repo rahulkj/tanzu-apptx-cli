3ef9ae0e119895b06d999beb0cd52115a361eac6
Rahul Jain <rahulkj@gmail.com>
2021-08-25 08:45:05 -0500

    global default

--- services/globaldefaults.go
+++ services/globaldefaults.go
@@ -1,7 +1,6 @@
 package services
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -13,35 +12,13 @@ type GlobalDefaults struct {
 	url       string
 	username  string
 	password  string
-	isDefault bool
 	sa_alias  string
 	sa_type   string
 	operation string
 }
 
-type ServiceAccountRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Alias    string `json:"alias"`
-}
-
-type ServiceAccountResponse struct {
-}
-
-type ServiceAccount struct {
-	UUID     string `json:"uuid"`
-	Alias    string `json:"alias"`
-	Username string `json:"username"`
-}
-
-type Response struct {
-	Embedded struct {
-		ServiceAccounts []struct {
-			UUID     string `json:"uuid"`
-			Alias    string `json:"alias"`
-			Username string `json:"username"`
-		} `json:"serviceAccounts"`
-	} `json:"_embedded"`
+type GlobalDefaultRequest struct {
+	ServiceAccountUUID string `json:"serviceAccountUUID"`
 }
 
 func (globalDefaults GlobalDefaults) Execute() {
@@ -51,10 +28,10 @@ func (globalDefaults GlobalDefaults) Execute() {
 	authResponse := Authenticate(request)
 
 	switch globalDefaults.operation {
-	case "create":
-		globalDefaults.createServiceAccount(authResponse.Token)
-	case "delete":
-		globalDefaults.deleteServiceAccount(authResponse.Token)
+	case "assign":
+		globalDefaults.assign(authResponse.Token)
+	case "reset":
+		globalDefaults.reset(authResponse.Token)
 	default:
 		fmt.Println("Operation not supported")
 		os.Exit(1)
@@ -67,83 +44,64 @@ func (globalDefaults GlobalDefaults) validate() GlobalDefaults {
 	username := inputCmd.String("username", "", "Iris admin username")
 	password := inputCmd.String("password", "", "Iris admin password")
 	sa_alias := inputCmd.String("sa_alias", "", "service account alias")
-	isDefault := inputCmd.Bool("default", false, "service account default (false)")
 	sa_type := inputCmd.String("service_account_type", "", "service account type, ex: VCs, VRNIs, LINUX_VMs")
-	operation := inputCmd.String("operation", "", "create, delete")
+	operation := inputCmd.String("operation", "", "assign, reset")
 
 	inputCmd.Parse(os.Args[2:])
 
 	if (*url == "" || *username == "" || *password == "") ||
-		(*operation == "" || *sa_alias == "") ||
-		(*isDefault && *sa_type == "") ||
+		(*operation == "" || *sa_type == "") ||
 		(strings.Contains(*url, "https://")) {
 		fmt.Println("subcommand 'globalDefault'")
 		inputCmd.PrintDefaults()
 		os.Exit(1)
 	}
 
-	globalDefaults = GlobalDefaults{*url, *username, *password, *isDefault, *sa_alias, *sa_type, *operation}
-	return globalDefaults
-}
-
-func (globalDefaults GlobalDefaults) createServiceAccount(token string) {
-	response := globalDefaults.findServiceAccount(globalDefaults.sa_alias, token)
-
-	if len(response.Embedded.ServiceAccounts) > 0 {
-		log.Println("Service Account already exists")
-	} else {
-		url := PROTOCOL + "://" + globalDefaults.url + "/" + PREFIX + "/" + SERVICE_ACCOUNTS + "?action=register"
-		request := ServiceAccountRequest{globalDefaults.username, globalDefaults.password, globalDefaults.sa_alias}
-		body, _ := processRequest(token, url, "POST", request)
-
-		serviceAccount := ServiceAccount{}
-		err := json.Unmarshal(body, &serviceAccount)
-		if err != nil {
-			log.Println("Failed to parse the response body.\n[ERROR] -", err)
-			os.Exit(1)
-		}
-
-		if len(serviceAccount.UUID) > 0 {
-			log.Println("Service Account created")
-		}
-	}
-}
-
-func (globalDefaults GlobalDefaults) findServiceAccount(alias string, token string) (response Response) {
-	url := PROTOCOL + "://" + globalDefaults.url + "/" + PREFIX + "/" + SERVICE_ACCOUNTS + "?page=0&size=10&alias=" + alias
-	body, _ := processRequest(token, url, "GET", nil)
-
-	err := json.Unmarshal(body, &response)
-	if err != nil {
-		log.Println("Failed to parse the response body.\n[ERROR] -", err)
+	if *operation == "assign" && *sa_alias == "" {
+		fmt.Println("subcommand 'globalDefault'")
+		inputCmd.PrintDefaults()
 		os.Exit(1)
 	}
 
-	log.Println(response)
-	return response
+	globalDefaults = GlobalDefaults{*url, *username, *password, *sa_alias, *sa_type, *operation}
+	return globalDefaults
 }
 
-func (globalDefaults GlobalDefaults) deleteServiceAccount(token string) {
-	response := globalDefaults.findServiceAccount(globalDefaults.sa_alias, token)
+func (globalDefaults GlobalDefaults) assign(token string) {
+	serviceAccounts := ServiceAccounts{}
+	response := serviceAccounts.findServiceAccount(globalDefaults.sa_alias, token)
 
 	if len(response.Embedded.ServiceAccounts) > 0 {
 
 		for _, serviceAccount := range response.Embedded.ServiceAccounts {
 			if serviceAccount.Alias == globalDefaults.sa_alias {
-				url := PROTOCOL + "://" + globalDefaults.url + "/" + PREFIX + "/" + SERVICE_ACCOUNTS + "/" + serviceAccount.UUID
-				_, responseCode := processRequest(token, url, "DELETE", nil)
+				url := PROTOCOL + "://" + globalDefaults.url + "/" + PREFIX + "/" + SERVICE_ACCOUNTS + "/defaults/" + globalDefaults.sa_type
+
+				request := GlobalDefaultRequest{serviceAccount.UUID}
+				_, responseCode := processRequest(token, url, "POST", request)
 
 				if responseCode == 200 {
-					log.Println("Deleted Service Account")
+					log.Println("Successfully assigned the service credential to the global default")
 				} else {
-					log.Println("Failed to delete Service Account")
+					log.Println("Failed to assign the service credential to the global default")
 				}
 			} else {
-				log.Println("Cannot delete Service Account as it does not exist")
+				log.Println("Cannot complete the operation as the Service Account does not exist")
 			}
 		}
 
 	} else {
-		log.Println("Cannot delete Service Account as it does not exist")
+		log.Println("Cannot complete the operation as the Service Account does not exist")
+	}
+}
+
+func (globalDefaults GlobalDefaults) reset(token string) {
+	url := PROTOCOL + "://" + globalDefaults.url + "/" + PREFIX + "/" + SERVICE_ACCOUNTS + "/defaults/" + globalDefaults.sa_type
+	_, responseCode := processRequest(token, url, "DELETE", nil)
+
+	if responseCode == 200 {
+		log.Println("Successfully reset the global default")
+	} else {
+		log.Println("Failed to reset the global default")
 	}
 }
